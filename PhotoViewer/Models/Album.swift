cbe7e1761f12b883ea82f5c5eecdf1db09f3f670
Jhantelleb <jhantelle.belleza@gmail.com>
2017-08-08 09:37:09 -0400

    Upload New files

---
+++ PhotoViewer/Models/Album.swift
@@ -0,0 +1,17 @@
+//
+//  Album.swift
+//  PhotoViewer
+//
+//  Created by Jhantelle Belleza on 8/7/17.
+//  Copyright © 2017 JhantelleB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AlbumModel {
+    
+    let albumID: Int
+    let backgroundColor: UIColor
+    
+}
